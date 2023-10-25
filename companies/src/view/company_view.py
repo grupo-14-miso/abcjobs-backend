d3cca0169224496c4e82c5aa18676d25b0ec6270
jgaray <jgaray@corelogic.com>
2023-10-25 18:48:35 -0500

    Activar build companies e interviews

--- companies/src/view/company_view.py
+++ companies/src/view/company_view.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 
 
+
 class VistaPing(Resource): 
     def get(self):
         return "Pong"
--- interviews/src/view/interview_view.py
+++ interviews/src/view/interview_view.py
@@ -1,7 +1,5 @@
 from flask_restful import Resource
 
-
-
 class VistaPing(Resource): 
     def get(self):
         return "Pong"
