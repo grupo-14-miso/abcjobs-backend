2cf2d3724600b361df100ef9bd34014c21af0f02
jgaray <jgaray@corelogic.com>
2023-10-25 18:37:12 -0500

    Corregir nombre de endpoint

--- companies/tests/view/test_company_view.py
+++ companies/tests/view/test_company_view.py
@@ -1,4 +1,4 @@
 
 def test_ping(client):
-        response_valid = client.get("/company/ping")
+        response_valid = client.get("/companies/ping")
         assert response_valid.json == "Pong"
--- interviews/tests/view/test_interview_view.py
+++ interviews/tests/view/test_interview_view.py
@@ -1,4 +1,4 @@
 
 def test_ping(client):
-        response_valid = client.get("/interview/ping")
+        response_valid = client.get("/interviews/ping")
         assert response_valid.json == "Pong"
