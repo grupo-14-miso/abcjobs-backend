0f431a01db644a8335b98c226d6442cf8c13fe4c
jgaray <jgaray@corelogic.com>
2023-10-30 22:36:33 -0500

    Companies: Post companies and offers

--- companies/main.py
+++ companies/main.py
@@ -2,15 +2,24 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+import firebase_admin
+from firebase_admin import credentials
+from google.oauth2 import service_account
 
-from src.view.company_view import VistaPing
+from src.view.company_view import VistaPing, CompanyView, OfferView
 
 app = Flask(__name__)
 
+cred = credentials.Certificate("./firebase.json")
+firebase_admin.initialize_app(cred)
+credentials = service_account.Credentials.from_service_account_file("./firebase.json")
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 api = Api(app)
 api.add_resource(VistaPing, "/companies/ping")
+api.add_resource(OfferView, "/companies/offer")
+api.add_resource(CompanyView, "/companies")
+
 
 
 cors = CORS(app)
--- companies/src/view/company_view.py
+++ companies/src/view/company_view.py
@@ -1,8 +1,80 @@
+import uuid
+from datetime import date
+
+from flask import request
 from flask_restful import Resource
+from google.cloud import datastore
 
-
+companies_entity = 'companies-data'
+offers_entity = 'offers-data'
 
 class VistaPing(Resource): 
     def get(self):
         return "Pong"
+
+
+class CompanyView(Resource):
+    def post(self):
+        data = request.get_json()
+
+        # Validate required fields
+        required_fields = []
+        for field in required_fields:
+            if field not in data:
+                return {'message': f'{field} is a required field'}, 400
+
+        # Initialize the Datastore client
+        client = datastore.Client()
+
+        key = client.key(companies_entity)
+        companies_ref = datastore.Entity(key=key)
+
+        #Build the company data
+        company_data = {
+            'company_id': str(uuid.uuid4()),  # Generate a unique ID
+            'document_type': data['document_type'],
+            'document_number': data['document_number'],
+            'name': data['name'],
+            'phone_number': data['phone_number'],
+            'country': data['country'],
+            'email': data['email'],
+            'created_date': date.today(),
+            'last_modified': date.today()
+        }
+
+        companies_ref.update(company_data)
+        client.put(companies_ref)
+        return {'message': 'Company created successfully', 'company_id': company_data['company_id']}, 201
+
+class OfferView(Resource):
+    def post(self):
+        data = request.get_json()
+
+        # Validate required fields
+        required_fields = []
+        for field in required_fields:
+            if field not in data:
+                return {'message': f'{field} is a required field'}, 400
+
+        # Initialize the Datastore client
+        client = datastore.Client()
+
+        key = client.key(offers_entity)
+        offers_ref = datastore.Entity(key=key)
+
+        # Build the company data
+        offers_data = {
+            'offer_id': str(uuid.uuid4()),  # Generate a unique ID
+            'company_id': data['company_id'],
+            'name': data['name'],
+            'description': data['description'],
+            'start_date': data['start_date'],
+            'end_date': data['end_date'],
+            'created_date': date.today(),
+            'last_modified': date.today()
+        }
+
+        offers_ref.update(offers_data)
+        client.put(offers_ref)
+        return {'message': 'Offer created successfully', 'offer_id': offers_data['offer_id']}, 201
 
