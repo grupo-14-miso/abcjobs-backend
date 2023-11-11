fefa08752b81d26f4db4cbcd6aeaa5639486d17c
jgaray <jgaray@corelogic.com>
2023-11-11 13:16:58 -0500

    Auth: Added unit testing

--- public/tests/utils/test_persistence.py
+++ public/tests/utils/test_persistence.py
@@ -1,6 +1,19 @@
 from src.utils.persistence import persist_new_candidate
+from unittest.mock import patch, MagicMock
+import pytest
+from main import app
 
+# Mock firebase_admin for tests
+firebase_admin = MagicMock()
+firebase_admin.initialize_app.return_value = None
 
+@pytest.fixture
+def client():
+    with patch('main.firebase_admin', firebase_admin):
+        with patch('google.auth.default', return_value=(None, None)):  # Mock GCP authentication
+            with patch('google.cloud.datastore.Client', autospec=True):  # Mock Datastore Client
+                with app.test_client() as client:
+                    yield client
 def test_create_user(client):
     # Perform a user persistence operation
     data = {
