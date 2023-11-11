4eff965dc11d99faf8a241ac827464075dc69af6
jgaray <jgaray@corelogic.com>
2023-11-11 11:40:40 -0500

    Auth: Added register and authentication for companies and candidates

---
+++ public/tests/utils/persistence.py
@@ -0,0 +1,65 @@
+from src.utils.persistence import persist_new_candidate
+
+
+def test_create_user(client):
+    # Perform a user persistence operation
+    data = {
+        "id_candidato": "correlation_id",
+        "email": "email",
+        "password": "password",
+        "Nombre": "nombre",
+        "apellido": "apellido",
+        "segundo_nombre": "segundo_nombre",
+        "segundo_apellido": "segundo_apellido",
+        "tipo_documento": "documento",
+        "documento": "documento",
+        "fecha_nacimiento": "01-12-1994",
+        "genero": "masculino|femenino|otro",
+        "nacionalidad": "nacionalidad",
+        "estado_civil": "soltero|casado|",
+        "telefono": "273635363",
+        "pais_nacimiento": "ecuador",
+        "pais_residencia": "Colombia",
+        "ciudad_nacimiento": "Guayaquil",
+        "lenguajes_programacion": ["", "", ""],
+        "tecnologias_herramientas": ["", "", ""],
+        "ciudad_residencia": "Ibague",
+        "educacion": [
+            {
+                "nivel_academico": "",
+                "institucion": "",
+                "titulo_obtenido": "",
+                "fecha_inicio": "",
+                "fecha_fin": "",
+                "rol": "",
+                "lenguajes_programacion": ["", "", ""],
+                "tecnologias_herramientas": ["", "", ""]
+            }
+        ],
+        "experiencia": [
+            {
+                "empresa": "",
+                "pais": "",
+                "ciudad": "",
+                "cargo": "",
+                "rol": "",
+                "fecha_inicio": "",
+                "fecha_fin": ""
+            }
+        ],
+        "idiomas": [
+            {
+                "idioma": "",
+                "nivel_conversacion": 20,
+                "nivel_lectura": 20,
+                "nivel_escritura": 20,
+                "nativo": "si",
+                "fecha_certificacion": ""
+            }
+        ]
+    }
+    response = persist_new_candidate(data, None, None)
+
+    # Validate the response
+    assert response is not None
+
